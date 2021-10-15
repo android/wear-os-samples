1dec4e31c2e68e733ea814aba7e9cb7d54409fe5
nikit19 <nikit.bhandari@gmail.com>
2021-10-15 22:17:55 +0530

    Use lower case package name

--- ComposeAdvanced/app/src/main/java/com/example/android/wearable/composeadvanced/presentation/ui/watchlist/WatchListViewModel.kt
+++ ComposeAdvanced/app/src/main/java/com/example/android/wearable/composeadvanced/presentation/ui/watchlist/WatchListViewModel.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.android.wearable.composeadvanced.presentation.ui.watch_list
+package com.example.android.wearable.composeadvanced.presentation.ui.watchlist
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
