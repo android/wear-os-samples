dbafcf64c4b6938abf5181acdc7a6cf7ed0bebb4
Yuri Schimke <yschimke@google.com>
2022-01-19 16:53:56 +0000

    Cleanup

--- ComposeAdvanced/app/src/main/java/com/example/android/wearable/composeadvanced/presentation/components/WatchAppChip.kt
+++ ComposeAdvanced/app/src/main/java/com/example/android/wearable/composeadvanced/presentation/components/WatchAppChip.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.Chip
