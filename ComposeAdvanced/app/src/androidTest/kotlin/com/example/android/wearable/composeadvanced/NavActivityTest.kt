c27b1b9e0379305cbc0a05db0d7e5e503026822a
Yuri Schimke <yuri@schimke.ee>
2022-04-06 15:22:04 +0300

    Fixes

--- ComposeAdvanced/app/src/androidTest/kotlin/com/example/android/wearable/composeadvanced/NavActivityTest.kt
+++ ComposeAdvanced/app/src/androidTest/kotlin/com/example/android/wearable/composeadvanced/NavActivityTest.kt
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      https://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,13 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.example.android.wearable.composeadvanced
 
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.lifecycle.Lifecycle
 import com.example.android.wearable.composeadvanced.presentation.MainActivity
 import com.example.android.wearable.composeadvanced.presentation.navigation.Screen
+import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 
@@ -43,10 +44,16 @@ class NavActivityTest {
     }
 
     private fun toListAndBack() {
+        assertEquals(Screen.Landing.route,
+            rule.activity.navController.currentBackStackEntry?.destination?.route)
+
         rule.runOnUiThread {
             rule.activity.navController.navigate(Screen.WatchList.route)
         }
         rule.waitForIdle()
+
+        assertEquals(Screen.WatchList.route,
+            rule.activity.navController.currentBackStackEntry?.destination?.route)
 
         rule.runOnUiThread {
             rule.activity.navController.navigate(Screen.Landing.route) {
@@ -54,5 +61,8 @@ class NavActivityTest {
             }
         }
         rule.waitForIdle()
+
+        assertEquals(Screen.Landing.route,
+            rule.activity.navController.currentBackStackEntry?.destination?.route)
     }
 }
