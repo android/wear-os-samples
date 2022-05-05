bdca24dd25b58ce574b79d3bd6df5023722b8777
jnichol <john.r.nichol@gmail.com>
2022-05-05 17:10:05 +0100

    Refactor Landingscreen
    Allow LandingScreen to take a map of menu item names to click handlers - before adding more items to the list I wanted to do a refactor and also check that folks are happy with this approach as I think we can apply it to some of the other menus as well.

--- ComposeAdvanced/app/src/main/java/com/example/android/wearable/composeadvanced/presentation/WearApp.kt
+++ ComposeAdvanced/app/src/main/java/com/example/android/wearable/composeadvanced/presentation/WearApp.kt
@@ -34,7 +34,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
