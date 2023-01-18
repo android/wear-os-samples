101ea4667f3478f70f3e0946338f316403e506f1
Yuri Schimke <yuri@schimke.ee>
2023-01-18 16:21:23 +0000

    Fix

--- WearOAuth/util/src/main/java/com/example/android/wearable/oauth/util/Requests.kt
+++ WearOAuth/util/src/main/java/com/example/android/wearable/oauth/util/Requests.kt
@@ -74,7 +74,7 @@ suspend fun doGetRequest(
 ): JSONObject {
     return withContext(dispatcher) {
         val conn = (URL(url).openConnection() as HttpURLConnection)
-        requestHeaders.forEach { (key, value) ->
+        requestHeaders.entries.forEach { (key, value) ->
             conn.setRequestProperty(key, value)
         }
         val inputReader = BufferedReader(InputStreamReader(conn.inputStream, "UTF-8"))
