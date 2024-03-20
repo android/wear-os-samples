875996c5f0c5b903324645290103ffbe23c4b050
Yuri Schimke <yschimke@google.com>
2024-03-20 08:35:04 +0000

    Use released screenshot code (#1044)

--- ComposeStarter/app/src/test/java/presentation/GreetingScreenTest.kt
+++ ComposeStarter/app/src/test/java/presentation/GreetingScreenTest.kt
@@ -18,6 +18,9 @@ package presentation
 import com.example.android.wearable.composestarter.presentation.GreetingScreen
 import com.google.android.horologist.compose.layout.AppScaffold
 import com.google.android.horologist.compose.layout.ResponsiveTimeText
+import com.google.android.horologist.screenshots.FixedTimeSource
+import com.google.android.horologist.screenshots.rng.WearDevice
+import com.google.android.horologist.screenshots.rng.WearScreenshotTest
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.ParameterizedRobolectricTestRunner
