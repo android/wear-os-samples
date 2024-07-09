20d85d6649a0b376f54025efa4fc9c01c056901a
Chiara Chiappini <cchiappini@google.com>
2024-07-09 10:44:49 +0100

    Use kotlin build.gradle in ComposeStarter

--- ComposeStarter/build.gradle.kts
+++ ComposeStarter/build.gradle.kts
@@ -16,7 +16,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     dependencies {
-        classpath libs.kotlin.gradle.plugin
+        classpath(libs.kotlin.gradle.plugin)
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -30,16 +30,19 @@ plugins {
 }
 
 subprojects {
-    apply plugin: "com.diffplug.spotless"
-
-    spotless {
+    apply(plugin = "com.diffplug.spotless")
+    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
         kotlin {
-            target "**/*.kt"
-            targetExclude("$buildDir/**/*.kt")
+            target("**/*.kt")
             targetExclude("bin/**/*.kt")
 
             ktlint(libs.versions.ktlint.get())
-            licenseHeaderFile rootProject.file("../spotless/copyright.kt")
+            licenseHeaderFile(rootProject.file("../spotless/copyright.kt"))
+        }
+
+        kotlinGradle {
+            target("*.gradle.kts")
+            ktlint()
         }
     }
 }
