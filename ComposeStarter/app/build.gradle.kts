c11413cfb5be2ea370ea8ede22d506d66df153c4
Chiara Chiappini <cchiappini@google.com>
2024-08-06 17:25:06 +0100

    adds 34

--- ComposeStarter/app/build.gradle.kts
+++ ComposeStarter/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
     defaultConfig {
         applicationId = "com.example.android.wearable.composestarter"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
     }
--- DataLayer/Application/build.gradle.kts
+++ DataLayer/Application/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         versionCode = 1
         versionName = "1.0"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
